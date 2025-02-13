c8dda72c1eecb3dcda35d29b24d9ef8869f254d9
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-02-12 22:06:03 -0800

    Refactor route generation code

--- src/routes.rs
+++ src/routes.rs
@@ -1,3 +1,16 @@
 pub mod csrf_tokens;
 pub mod sessions;
 pub mod users;
+
+
+use axum::{Router, routing::{get, post}};
+
+
+pub fn app() -> Router {
+    Router::new()
+        .route("/users", post(self::users::post))
+        .route("/sessions", post(self::sessions::post))
+        .route("/sessions/{uuid}", get(self::sessions::get))
+        .route("/csrf_tokens", post(self::csrf_tokens::post))
+        .route("/csrf_tokens/{token}", get(self::csrf_tokens::get))
+}
